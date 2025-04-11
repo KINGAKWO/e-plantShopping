df409431cf374df4eec40de07d563453dbbca60f
KINGAKWO <kingakwomakembe@gmail.com>
2025-04-11 23:01:17 +0100

    productList component layout

--- src/store.js
+++ src/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    products: (state = [], action) => {
+      switch (action.type) {
+        case 'ADD_PRODUCT':
+          return [...state, { ...action.payload }];
+        default:
+          return state;
+      }
     },
+  },
 });
-export default store
+export default store;
